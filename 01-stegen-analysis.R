d3dec63e3d63a5d7cc295ba8d91b8dc877c604ec
DrJohan <johan@ms.pmc.edu.my>
2018-12-20 20:53:02 +0800

    Cleaning and saving new dataset

--- 01-stegen-analysis.R
+++ 01-stegen-analysis.R
@@ -23,7 +23,7 @@ View(stegen)
 
 
 
-##Starting to do some exploratory analysis
+##Starting to do some preliminary exploratory analysis
 
 dim(stegen)
 names(stegen)
@@ -36,3 +36,44 @@ table(stegen$tiramisu)
 table(stegen$tiramisu, useNA = "no")
 table(stegen$tiramisu, useNA = "ifany")
 table(stegen$tiramisu, useNA = "always")
+
+
+## Doing some data cleaning before analysis
+
+stegen_old <- stegen
+new_labels <- clean_labels(names(stegen))
+new_labels
+names(stegen) <- new_labels
+
+stegen$unique_key <- as.character(stegen$unique_key)
+stegen$sex <- factor(stegen$sex)
+stegen$ill <- factor(stegen$ill)
+stegen$date_onset <- as.Date(stegen$date_onset)
+
+
+stegen$sex <- recode_factor(stegen$sex, "0" = "male", "1" = "female")
+stegen$ill <- recode_factor(stegen$ill, "0" = "non case", "1" = "case")
+
+str(stegen$sex)
+str(stegen$ill)
+
+table(stegen$pork, useNA = "always")
+table(stegen$salmon, useNA = "always")
+table(stegen$horseradish, useNA = "always")
+
+
+stegen$pork[stegen$pork == 9] <- NA
+stegen$salmon[stegen$salmon == 9] <- NA
+stegen$horseradish[stegen$horseradish == 9] <- NA
+
+## Saving cleaning data into new directory
+
+clean_dir <- here("data", "cleaned")
+dir.create(clean_dir)
+
+stegen_clean_file <- here("data", "cleaned", "stegen_clean.csv")
+write_csv(stegen, path = stegen_clean_file)
+
+
+stegen_clean_rds <- here("data", "cleaned", "stegen-clean.rds")
+saveRDS(stegen, file = stegen_clean_rds)
